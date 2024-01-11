5b0986d2bf06d5c2d5b03522c89b4ecc4bb6a125
project-java-reza <rizqirezaardianyahjava@gmail.com>
2024-01-11 07:02:33 +0700

    Initial Commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import Link from 'next/link';
+
 export default function Home() {
-  return <p>Hello World</p>;
+  return (
+    <MaxWidthWrapper>
+      <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
+        <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+          Your marketplace for high-quality{" "}
+          <span className="text-blue-600">digital assets</span>.
+        </h1>
+        <p className="mt-6 text-lg max-w-prose text-muted-foreground">
+          Welcome to DigitalReza. Every asset on our platform is verified by our
+          team to ensure highest quality standards.
+        </p>
+        <div className="flex flex-col sm:flex-row gap-4 mt-6">
+          <Link href='/products' >Browse Trending</Link>
+        </div>
+      </div>
+    </MaxWidthWrapper>
+  );
 }
